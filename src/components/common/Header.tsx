158830ba935f2e8ffd827764ee71246ea966215f
Charlie <itscharlieliu@gmail.com>
2020-05-29 16:43:44 -0700

    Added header component

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Header = styled.div`
+    font-size: 2em;
+    font-weight: 500;
+`;
+
+export default Header;
--- src/components/screens/BudgetScreen.tsx
+++ src/components/screens/BudgetScreen.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import { UNIT_LENGTH } from "../../defs/theme";
+import Header from "../common/Header";
 
 const ScreenContainer = styled.div`
     padding: ${UNIT_LENGTH}px;
 `;
 
 const BudgetScreen = (): JSX.Element => {
-    return <ScreenContainer>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</ScreenContainer>;
+    return (
+        <ScreenContainer>
+            <Header>Budget</Header>
+        </ScreenContainer>
+    );
 };
 
 export default BudgetScreen;
