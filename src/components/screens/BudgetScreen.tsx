c5e5d4e9fc8475c66b5c27e1cb142f7f04255f6f
Charlie <itscharlieliu@gmail.com>
2020-06-04 17:54:36 -0700

    Added budget container in budget screen

--- src/components/screens/BudgetScreen.tsx
+++ src/components/screens/BudgetScreen.tsx
@@ -2,19 +2,62 @@ import React from "react";
 import { connect, ResolveThunks } from "react-redux";
 import styled from "styled-components";
 
-import { UNIT_LENGTH } from "../../defs/theme";
+import { theme, UNIT_LENGTH } from "../../defs/theme";
 import IApplicationState from "../../store";
 import { updateBudget } from "../../store/budget/budgetActions";
-import { IBudgetCategory, IMonthlyBudget, ITotalBudget } from "../../store/budget/budgetInterfaces";
+import { ITotalBudget } from "../../store/budget/budgetInterfaces";
 import Button from "../common/Button";
 
-const BudgetContainer = (): JSX.Element => {
-    return <div>Container</div>;
-};
-
 const ScreenContainer = styled.div`
+    flex-grow: 1;
     padding: ${UNIT_LENGTH / 2}px;
 `;
+
+const BudgetContainer = styled.div`
+    display: grid;
+    width: auto;
+    grid-template-columns: auto 15% 15% 15%;
+`;
+
+const BudgetHeaderText = styled.span`
+    font-size: ${theme.font.size.big};
+    font-weight: ${theme.font.weight.bold};
+    padding: 16px;
+`;
+
+const BudgetCategoryText = styled.span`
+    font-size: ${theme.font.size.small};
+    padding: 16px;
+    border-top: 1px solid ${theme.palette.divider.main};
+`;
+
+const BudgetHeader = (): JSX.Element => {
+    return (
+        <>
+            <BudgetHeaderText>Category</BudgetHeaderText>
+            <BudgetHeaderText>Budgeted</BudgetHeaderText>
+            <BudgetHeaderText>Activity</BudgetHeaderText>
+            <BudgetHeaderText>Remaining</BudgetHeaderText>
+        </>
+    );
+};
+
+interface IBudgetRowProps {
+    category: string;
+    budgeted: number;
+    activity: number;
+}
+
+const BudgetRow = (props: IBudgetRowProps): JSX.Element => {
+    return (
+        <>
+            <BudgetCategoryText>{props.category}</BudgetCategoryText>
+            <BudgetCategoryText>{props.budgeted}</BudgetCategoryText>
+            <BudgetCategoryText>{props.activity}</BudgetCategoryText>
+            <BudgetCategoryText>{props.budgeted + props.activity}</BudgetCategoryText>
+        </>
+    );
+};
 
 interface IStateProps {
     totalBudget: ITotalBudget;
@@ -29,9 +72,11 @@ type TAllProps = IStateProps & ResolveThunks<IDispatchProps>;
 const BudgetScreen = (props: TAllProps): JSX.Element => {
     return (
         <ScreenContainer>
-            <Header>Budget</Header>
             <Button onClick={() => props.updateBudget(new Date(), "test", "test", 3, 3)}>Hello</Button>
-            <BudgetContainer />
+            <BudgetContainer>
+                <BudgetHeader />
+                <BudgetRow category={"test1"} budgeted={100} activity={-60} />
+            </BudgetContainer>
         </ScreenContainer>
     );
 };
--- src/defs/theme.ts
+++ src/defs/theme.ts
@@ -33,11 +33,19 @@ export const theme = {
             "box-shadow: 0px 4px 8px 0px rgba(0,0,0,0.5);",
     },
     font: {
-        biggest: "2em",
-        bigger: "1.5em",
-        big: "1.17em",
-        small: "1em",
-        smaller: ".83em",
-        smallest: ".67em",
+        size: {
+            biggest: "2em",
+            bigger: "1.5em",
+            big: "1.17em",
+            small: "1em",
+            smaller: ".83em",
+            smallest: ".67em",
+        },
+        weight: {
+            boldest: 900,
+            bolder: 700,
+            bold: 600,
+            none: 500,
+        },
     },
 };
