011274626933c961fca2232e07fed16a1230f8b7
Charlie <itscharlieliu@gmail.com>
2020-06-13 20:59:31 -0700

    Added animation on modal show

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 import { theme } from "../../defs/theme";
 
+// TODO Create better animation for input on focus
+
 const Input = styled.input`
     outline: none;
     border-width: 2px;
     border-style: none none solid none;
     border-color: ${theme.palette.input.inactive};
+    padding: 8px 0;
 
     &:focus {
         border-color: ${theme.palette.input.active};
--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,8 +1,9 @@
-import React, { ReactNode, useRef } from "react";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 import { theme, Z_INDEX_MODAL } from "../../defs/theme";
 import useOutsideClick from "../../utils/useOutsideClick";
+import useMount from "../../utils/useMount";
 
 interface ModalProps {
     visible?: boolean;
@@ -10,7 +11,11 @@ interface ModalProps {
     onClose?: () => void;
 }
 
-const ModalBackground = styled.div`
+interface ModalBackgroundProps {
+    opacity: number;
+}
+
+const ModalBackground = styled.div<ModalBackgroundProps>`
     width: 100vw;
     height: 100vh;
     position: absolute;
@@ -21,6 +26,9 @@ const ModalBackground = styled.div`
     justify-content: center;
     align-items: center;
     background-color: ${theme.palette.modal.background};
+    opacity: ${(props: ModalBackgroundProps) => props.opacity};
+
+    transition: opacity 0.2s;
 `;
 
 const ModalContainer = styled.div`
@@ -30,7 +38,14 @@ const ModalContainer = styled.div`
 `;
 
 const Modal = (props: ModalProps): JSX.Element | null => {
+    const [fadedIn, setFadedIn] = useState<boolean>(false);
+    const [isShowing, setIsShowing] = useState<boolean>(false);
+
+    const visible = props.visible === undefined ? true : props.visible;
+
     const modalRef = useRef<HTMLDivElement>(null);
+
+    console.log("rerendered");
 
     useOutsideClick(
         modalRef,
@@ -38,11 +53,29 @@ const Modal = (props: ModalProps): JSX.Element | null => {
         props.visible && props.onClose !== undefined,
     );
 
-    if (props.visible !== undefined && !props.visible) {
+    // Initialize opacity at 0, so we fade in when user activates component
+    useEffect(() => {
+        if (visible) {
+            const timeout = setTimeout(() => {
+                setIsShowing(true);
+                setFadedIn(true);
+            }, 50);
+            return () => clearTimeout(timeout);
+        }
+        const fadeTimeout = setTimeout(() => setFadedIn(false), 50);
+        const showTimeout = setTimeout(() => setIsShowing(false), 200);
+        return () => {
+            clearTimeout(fadeTimeout);
+            clearTimeout(showTimeout);
+        };
+    }, [visible]);
+
+    if (!visible && !isShowing) {
         return null;
     }
+
     return (
-        <ModalBackground>
+        <ModalBackground opacity={fadedIn ? 1 : 0} onAnimationEnd={() => console.log("test")}>
             <ModalContainer ref={modalRef}>{props.children}</ModalContainer>
         </ModalBackground>
     );
