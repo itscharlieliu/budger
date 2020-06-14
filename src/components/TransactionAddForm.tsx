4e6fdd166405ecf06955f7283aa85b41ba3d4678
Charlie <itscharlieliu@gmail.com>
2020-06-14 14:31:23 -0700

    Adding state to add transaction setup

--- src/components/TransactionAddForm.tsx
+++ src/components/TransactionAddForm.tsx
@@ -1,8 +1,16 @@
 import styled from "styled-components";
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import Input from "./common/Input";
 import t from "../services/i18n/language";
 import Button from "./common/Button";
+import { addTransaction } from "../store/transactions/transactionActions";
+import { connect, ResolveThunks } from "react-redux";
+
+interface DispatchProps {
+    addTransaction: typeof addTransaction;
+}
+
+type AllProps = ResolveThunks<DispatchProps>;
 
 const TransactionAddContainer = styled.div`
     padding: 16px;
@@ -11,20 +19,65 @@ const TransactionAddContainer = styled.div`
     align-items: center;
 `;
 
-const TransactionAddForm = (): JSX.Element => {
+const TransactionAddForm = (props: AllProps): JSX.Element => {
+    const [toFrom, setToFrom] = useState<string>("");
+    const [account, setAccount] = useState<string>("");
+    const [category, setCategory] = useState<string>("");
+    const [date, setDate] = useState<string>("");
+    const [inflow, setInflow] = useState<number>(0);
+    const [outflow, setOutflow] = useState<number>(0);
+    const [note, setNote] = useState<string>("");
+
+    const handleAddButtonPress = () => {
+        props.addTransaction(toFrom, account, category, new Date(date), inflow - outflow, note);
+    };
+
     return (
         <TransactionAddContainer>
-            <Input placeholder={t("toFrom")} />
-            <Input placeholder={t("account")} />
-            <Input placeholder={t("category")} />
-            <Input placeholder={t("date")} />
-            <Input placeholder={t("in")} />
-            <Input placeholder={t("out")} />
-            <Input placeholder={t("notes")} />
-            <Button>add</Button>
+            <Input
+                placeholder={t("toFrom")}
+                value={toFrom}
+                onChange={(event: FormEvent) => setToFrom((event.target as HTMLInputElement).value)}
+            />
+            <Input
+                placeholder={t("account")}
+                value={account}
+                onChange={(event: FormEvent) => setAccount((event.target as HTMLInputElement).value)}
+            />
+            <Input
+                placeholder={t("category")}
+                value={category}
+                onChange={(event: FormEvent) => setCategory((event.target as HTMLInputElement).value)}
+            />
+            <Input
+                placeholder={t("date")}
+                value={date}
+                onChange={(event: FormEvent) => setDate((event.target as HTMLInputElement).value)}
+            />
+            <Input
+                placeholder={t("in")}
+                type={"number"}
+                value={inflow}
+                onChange={(event: FormEvent) => setInflow(parseInt((event.target as HTMLInputElement).value))}
+            />
+            <Input
+                placeholder={t("out")}
+                value={outflow}
+                onChange={(event: FormEvent) => setOutflow(parseInt((event.target as HTMLInputElement).value))}
+            />
+            <Input
+                placeholder={t("notes")}
+                value={note}
+                onChange={(event: FormEvent) => setNote((event.target as HTMLInputElement).value)}
+            />
+            <Button onClick={handleAddButtonPress}>add</Button>
             <Button>cancel</Button>
         </TransactionAddContainer>
     );
 };
 
-export default TransactionAddForm;
+const mapDispatchToProps = {
+    addTransaction,
+};
+
+export default connect(null, mapDispatchToProps)(TransactionAddForm);
