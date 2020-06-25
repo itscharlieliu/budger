db8da0fc51aeada3ef9f21044b681295bcc51245
Charlie <itscharlieliu@gmail.com>
2020-06-25 16:23:40 -0700

    Styled date selector

--- src/components/common/DateSelector.tsx
+++ src/components/common/DateSelector.tsx
@@ -1,20 +1,20 @@
 import React, { useEffect, useRef, useState } from "react";
-import "react-day-picker/lib/style.css";
+import "./styles/DateSelector.css";
 import DayPickerInput from "react-day-picker/DayPickerInput";
 import { DayPickerInputProps } from "react-day-picker";
 import styled from "styled-components";
 import { ZIndex } from "../../defs/theme";
 
-const StyledDataPickerInput = styled(DayPickerInput)`
-    background-color: red;
-    .DayPickerInput-Overlay {
-        background-color: blue;
-        z-index: ${ZIndex.calendar};
-    }
+const DatePickerContainer = styled.div`
+    --z-index: ${ZIndex.calendar};
 `;
 
 const DateSelector = (props: DayPickerInputProps): JSX.Element => {
-    return <StyledDataPickerInput placeholder="DD/MM/YYYY" format="DD/MM/YYYY" {...props} />;
+    return (
+        <DatePickerContainer>
+            <DayPickerInput placeholder="DD/MM/YYYY" format="DD/MM/YYYY" {...props} />
+        </DatePickerContainer>
+    );
 };
 
 export default DateSelector;
