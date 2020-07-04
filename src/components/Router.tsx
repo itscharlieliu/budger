9634c5c083a7ec213d4d6a8280660aa521a3afe4
Charlie <itscharlieliu@gmail.com>
2020-07-04 12:59:02 -0700

    Added account screen

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 
-import { BUDGET, TRANSACTIONS } from "../defs/routerPaths";
+import { ACCOUNTS, BUDGET, TRANSACTIONS } from "../defs/routerPaths";
 
 import BudgetScreen from "./screens/BudgetScreen";
 import TransactionsScreen from "./screens/TransactionsScreen";
+import AccountScreens from "./screens/AccountsScreen";
 
 const Router = (): JSX.Element => {
     return (
         <Switch>
             <Route path={BUDGET} exact component={BudgetScreen} />
             <Route path={TRANSACTIONS} exact component={TransactionsScreen} />
+            <Route path={ACCOUNTS} exact component={AccountScreens} />
             <Redirect to={BUDGET} />
         </Switch>
     );
