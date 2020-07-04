d9b24590f0e97a19b168c39cf05e12eacf4dcb7a
Charlie <itscharlieliu@gmail.com>
2020-07-03 19:09:55 -0700

    Added more tests for redux

--- src/store/__tests__/budget.test.ts
+++ src/store/__tests__/budget.test.ts
@@ -1,36 +1,163 @@
 import { AnyAction } from "redux";
-import configureMockStore, { MockStoreEnhanced } from "redux-mock-store";
+import configureMockStore from "redux-mock-store";
 import thunk, { ThunkDispatch } from "redux-thunk";
 
-import { addBudgetGroup } from "../budget/budgetActions";
-import { ADD_BUDGET_GROUP_SUCCESS, ADDING_BUDGET_GROUP } from "../budget/budgetInterfaces";
+import { addBudgetCategory, addBudgetGroup } from "../budget/budgetActions";
+import {
+    ADD_BUDGET_CATEGORY_FAILURE,
+    ADD_BUDGET_CATEGORY_SUCCESS,
+    ADD_BUDGET_GROUP_FAILURE,
+    ADD_BUDGET_GROUP_SUCCESS,
+    ADDING_BUDGET_CATEGORY,
+    ADDING_BUDGET_GROUP,
+    TotalBudget,
+} from "../budget/budgetInterfaces";
 import budgetReducer from "../budget/budgetReducer";
 import ApplicationState from "../index";
-import rootReducer from "../rootReducer";
 
 type Dispatch = ThunkDispatch<ApplicationState, null, AnyAction>;
 
-const mockStore = configureMockStore<ApplicationState, Dispatch>([thunk]);
+const mockStore = configureMockStore<unknown, Dispatch>([thunk]);
 
 describe("budget actions", () => {
-    let budgetStore: MockStoreEnhanced<ApplicationState, Dispatch>;
-
-    beforeEach(() => {
-        budgetStore = mockStore(rootReducer(undefined, { type: undefined }));
-    });
-
     it("successfully adds budget group", async () => {
-        await budgetStore.dispatch(addBudgetGroup("test group"));
-        const actions = budgetStore.getActions();
+        const store = mockStore({
+            budget: {
+                totalBudget: [],
+            },
+        });
+        await store.dispatch(addBudgetGroup("test group"));
+        const actions = store.getActions();
 
         expect(actions[0].type).toBe(ADDING_BUDGET_GROUP);
         expect(actions[1].type).toBe(ADD_BUDGET_GROUP_SUCCESS);
     });
+
+    it("does not add duplicate budget group", async () => {
+        const store = mockStore({
+            budget: {
+                totalBudget: [
+                    {
+                        group: "test group",
+                    },
+                ],
+            },
+        });
+
+        await store.dispatch(addBudgetGroup("test group"));
+
+        const actions = store.getActions();
+
+        expect(actions[0].type).toBe(ADDING_BUDGET_GROUP);
+        expect(actions[1].type).toBe(ADD_BUDGET_GROUP_FAILURE);
+    });
+
+    it("successfully adds category", async () => {
+        const store = mockStore({
+            budget: {
+                totalBudget: [
+                    {
+                        group: "test group",
+                        categories: [],
+                    },
+                ],
+            },
+        });
+        await store.dispatch(addBudgetCategory("test group", "test category"));
+        const actions = store.getActions();
+
+        expect(actions[0].type).toBe(ADDING_BUDGET_CATEGORY);
+        expect(actions[1].type).toBe(ADD_BUDGET_CATEGORY_SUCCESS);
+    });
+
+    it("does not add budget category if budget group does not exist", async () => {
+        const store = mockStore({
+            budget: {
+                totalBudget: [],
+            },
+        });
+        await store.dispatch(addBudgetCategory("test group", "test category"));
+        const actions = store.getActions();
+
+        expect(actions[0].type).toBe(ADDING_BUDGET_CATEGORY);
+        expect(actions[1].type).toBe(ADD_BUDGET_CATEGORY_FAILURE);
+    });
+
+    it("does not add budget category if budget category already exists", async () => {
+        const store = mockStore({
+            budget: {
+                totalBudget: [
+                    {
+                        group: "test group",
+                        categories: [
+                            {
+                                category: "test category",
+                            },
+                        ],
+                    },
+                ],
+            },
+        });
+        await store.dispatch(addBudgetCategory("test group", "test category"));
+        const actions = store.getActions();
+
+        expect(actions[0].type).toBe(ADDING_BUDGET_CATEGORY);
+        expect(actions[1].type).toBe(ADD_BUDGET_CATEGORY_FAILURE);
+    });
 });
 
 describe("budget reducer", () => {
-    it("adds budget group", async () => {
+    it("adds budget group", () => {
         const budgetState = budgetReducer(undefined, { type: ADDING_BUDGET_GROUP });
         expect(budgetState.isAddingBudgetGroup).toEqual(true);
+
+        const totalBudget: TotalBudget = [
+            {
+                group: "test group",
+                categories: [
+                    {
+                        category: "test category",
+                        budgeted: 1999,
+                        activity: 200,
+                    },
+                ],
+            },
+        ];
+
+        const successBudgetState = budgetReducer(budgetState, { type: ADD_BUDGET_GROUP_SUCCESS, totalBudget });
+        expect(successBudgetState.isAddingBudgetGroup).toEqual(false);
+        expect(successBudgetState.totalBudget).toEqual(totalBudget);
+
+        const error = new Error("test group error");
+        const failureBudgetState = budgetReducer(budgetState, { type: ADD_BUDGET_GROUP_FAILURE, error });
+        expect(failureBudgetState.isAddingBudgetGroup).toEqual(false);
+        expect(failureBudgetState.error).toEqual(error);
+    });
+
+    it("adds budget category", () => {
+        const budgetState = budgetReducer(undefined, { type: ADDING_BUDGET_CATEGORY });
+        expect(budgetState.isAddingBudgetCategory).toEqual(true);
+
+        const totalBudget: TotalBudget = [
+            {
+                group: "test group",
+                categories: [
+                    {
+                        category: "test category",
+                        budgeted: 1999,
+                        activity: 200,
+                    },
+                ],
+            },
+        ];
+
+        const successBudgetState = budgetReducer(budgetState, { type: ADD_BUDGET_CATEGORY_SUCCESS, totalBudget });
+        expect(successBudgetState.isAddingBudgetCategory).toEqual(false);
+        expect(successBudgetState.totalBudget).toEqual(totalBudget);
+
+        const error = new Error("test category error");
+        const failureBudgetState = budgetReducer(budgetState, { type: ADD_BUDGET_CATEGORY_FAILURE, error });
+        expect(failureBudgetState.isAddingBudgetCategory).toEqual(false);
+        expect(failureBudgetState.error).toEqual(error);
     });
 });
