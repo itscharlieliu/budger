623e6aa9f3066b323aeec64cf5d099342cd84b2b
Charlie <itscharlieliu@gmail.com>
2020-07-06 18:18:01 -0700

    Create form for adding account

--- src/components/forms/AccountAddForm.tsx
+++ src/components/forms/AccountAddForm.tsx
@@ -4,8 +4,10 @@ import { connect, ResolveThunks } from "react-redux";
 
 import t from "../../services/i18n/language";
 import { addAccount } from "../../store/accounts/accountsActions";
+import { AccountType } from "../../store/accounts/accountsInterfaces";
 import Button from "../common/Button";
 import Input from "../common/Input";
+import Switch from "../common/Switch";
 import ModalFormContainer from "../common/containers/ModalFormContainer";
 
 interface OwnProps {
@@ -20,16 +22,43 @@ type AllProps = OwnProps & ResolveThunks<DispatchProps>;
 
 interface FormValues {
     accountName?: string;
+    budgeted?: boolean;
+    startingBalance?: string;
+}
+
+interface FormErrors {
+    accountName?: string;
+    budgeted?: string;
+    startingBalance?: string;
 }
 
 const AccountAddForm = (props: AllProps): JSX.Element => {
     const handleAddAccount = (values: FormValues) => {
-        values.accountName && props.addAccount(values.accountName);
+        const startingBalace = parseFloat(values.startingBalance ? values.startingBalance : "0");
+
+        values.accountName &&
+            props.addAccount(
+                values.accountName,
+                values.budgeted ? AccountType.budgeted : AccountType.unbudgeted,
+                isNaN(startingBalace) ? 0 : startingBalace,
+            );
         props.onSubmit && props.onSubmit();
     };
+
+    const handleValidation = (values: FormValues) => {
+        const errors: FormErrors = {};
+
+        if (!values.accountName) {
+            errors.accountName = t("cannotBeEmpty");
+        }
+
+        return errors;
+    };
+
     return (
         <Form
             onSubmit={handleAddAccount}
+            validate={handleValidation}
             component={({ handleSubmit }: FormRenderProps) => (
                 <ModalFormContainer onSubmit={handleSubmit}>
                     <Field name={"accountName"}>
@@ -40,6 +69,21 @@ const AccountAddForm = (props: AllProps): JSX.Element => {
                                 error={meta.touched && meta.error}
                                 label={t("accountName")}
                                 autoFocus
+                            />
+                        )}
+                    </Field>
+                    <Field name={"budgeted"} type={"checkbox"}>
+                        {({ input, meta }: FieldRenderProps<string, HTMLElement>) => (
+                            <Switch {...input} spaced error={meta.touched && meta.error} label={t("budgeted")} />
+                        )}
+                    </Field>
+                    <Field name={"startingBalance"}>
+                        {({ input, meta }: FieldRenderProps<string, HTMLElement>) => (
+                            <Input
+                                {...input}
+                                helperText={meta.touched && meta.error}
+                                error={meta.touched && meta.error}
+                                label={t("startingBalance")}
                             />
                         )}
                     </Field>
