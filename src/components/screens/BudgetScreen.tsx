15e86d347677886b53327aa694f46c72c8a4e3c8
Charlie <itscharlieliu@gmail.com>
2020-07-19 11:36:18 -0700

    Add ability to delete budget category

--- src/components/screens/BudgetScreen.tsx
+++ src/components/screens/BudgetScreen.tsx
@@ -7,7 +7,6 @@ import { ReactComponent as PlusIcon } from "../../resources/images/plusIcon.svg"
 import { ReactComponent as Trash } from "../../resources/images/trash.svg";
 import t from "../../services/i18n/language";
 import ApplicationState from "../../store";
-import { updateBudget } from "../../store/budget/budgetActions";
 import { BudgetCategory, BudgetGroup, TotalBudget } from "../../store/budget/budgetInterfaces";
 import Button from "../common/Button";
 import Modal from "../common/Modal";
@@ -15,16 +14,26 @@ import GridHeaderContainer from "../common/containers/GridHeaderContainer";
 import ScreenContainer from "../common/containers/ScreenContainer";
 import BudgetCategoryAddForm from "../forms/BudgetCategoryAddForm";
 import BudgetGroupAddForm from "../forms/BudgetGroupAddForm";
+import { deleteBudgetCategory } from "../../store/budget/budgetActions";
 
 interface StateProps {
     totalBudget: TotalBudget;
 }
 
 interface DispatchProps {
-    updateBudget: typeof updateBudget;
+    deleteBudgetCategory: typeof deleteBudgetCategory;
 }
 
 type AllProps = StateProps & ResolveThunks<DispatchProps>;
+
+interface BudgetCategoryRowProps extends BudgetCategory {
+    onDelete: (category: string) => void;
+}
+
+interface BudgetGroupRowProps extends BudgetGroup {
+    onDeleteCategory: (category: string) => void;
+    // onDeleteGroup: () => void;
+}
 
 const BudgetContainer = styled.div`
     display: grid;
@@ -49,7 +58,10 @@ const BudgetGroupContainer = styled.div`
     grid-column-end: 5;
 `;
 
-const BudgetCategoryText = styled.span`
+const BudgetCategoryContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
     font-size: ${theme.font.size.small};
     padding: 16px;
     border-top: 1px solid ${theme.palette.divider.main};
@@ -74,19 +86,24 @@ const BudgetHeader = (): JSX.Element => {
     );
 };
 
-const BudgetCategoryRow = (props: BudgetCategory): JSX.Element => {
+const BudgetCategoryRow = (props: BudgetCategoryRowProps): JSX.Element => {
     return (
         <>
-            <BudgetCategoryText>{props.category}</BudgetCategoryText>
-            <BudgetCategoryText>{props.budgeted}</BudgetCategoryText>
-            <BudgetCategoryText>{props.activity}</BudgetCategoryText>
-            <BudgetCategoryText>{props.budgeted + props.activity}</BudgetCategoryText>
+            <BudgetCategoryContainer>
+                {props.category}
+                <BudgetAddButton icon={<Trash />} onClick={() => props.onDelete(props.category)} flat />
+            </BudgetCategoryContainer>
+            <BudgetCategoryContainer>{props.budgeted}</BudgetCategoryContainer>
+            <BudgetCategoryContainer>{props.activity}</BudgetCategoryContainer>
+            <BudgetCategoryContainer>{props.budgeted + props.activity}</BudgetCategoryContainer>
         </>
     );
 };
 
-const BudgetGroupRow = (props: BudgetGroup): JSX.Element => {
+const BudgetGroupRow = (props: BudgetGroupRowProps): JSX.Element => {
     const [isAddingCategory, setIsAddingCategory] = useState(false);
+
+    console.log(props.categories);
 
     return (
         <>
@@ -96,7 +113,6 @@ const BudgetGroupRow = (props: BudgetGroup): JSX.Element => {
                 </Modal>
                 {props.group}
                 <BudgetAddButton icon={<PlusIcon />} onClick={() => setIsAddingCategory(true)} flat />
-                <BudgetAddButton icon={<Trash />} onClick={() => setIsAddingCategory(true)} flat />
             </BudgetGroupContainer>
             {props.categories.map((category: BudgetCategory) => (
                 <BudgetCategoryRow
@@ -104,6 +120,7 @@ const BudgetGroupRow = (props: BudgetGroup): JSX.Element => {
                     category={category.category}
                     budgeted={category.budgeted}
                     activity={category.activity}
+                    onDelete={props.onDeleteCategory}
                 />
             ))}
         </>
@@ -116,7 +133,11 @@ const BudgetScreen = (props: AllProps): JSX.Element => {
             <BudgetContainer>
                 <BudgetHeader />
                 {props.totalBudget.map((budgetGroup: BudgetGroup, index: number) => (
-                    <BudgetGroupRow key={"budgetGroup" + index} {...budgetGroup} />
+                    <BudgetGroupRow
+                        key={"budgetGroup" + index}
+                        {...budgetGroup}
+                        onDeleteCategory={props.deleteBudgetCategory}
+                    />
                 ))}
             </BudgetContainer>
         </ScreenContainer>
@@ -128,7 +149,7 @@ const mapStateToProps = (state: ApplicationState): StateProps => ({
 });
 
 const mapDispatchToProps: DispatchProps = {
-    updateBudget,
+    deleteBudgetCategory,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(BudgetScreen);
