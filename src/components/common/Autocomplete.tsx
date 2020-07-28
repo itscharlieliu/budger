1bd6115fd44d67476da79f8cfe3eeb19edd149af
Charlie <itscharlieliu@gmail.com>
2020-07-27 17:07:52 -0700

    Ease of use: move on to next input once option selected

--- src/components/common/Autocomplete.tsx
+++ src/components/common/Autocomplete.tsx
@@ -1,5 +1,5 @@
 import { useCombobox, UseComboboxState } from "downshift";
-import React from "react";
+import React, { Ref, RefObject } from "react";
 import { Omit } from "react-redux";
 import styled from "styled-components";
 
@@ -43,74 +43,78 @@ const DropDownOption = styled.div<DropDownOptionProps>`
         props.highlight ? theme.palette.primary.contrast : theme.palette.background.contrast};
 `;
 
-const Autocomplete = (props: AutocompleteProps): JSX.Element => {
-    const { options, value, onFocus, onChange, onSelectedItemChange, ...otherInputProps } = props;
+const Autocomplete = React.forwardRef(
+    (props: AutocompleteProps, ref: Ref<HTMLInputElement>): JSX.Element => {
+        const { options, value, onFocus, onChange, onSelectedItemChange, ...otherInputProps } = props;
 
-    const itemToString = (item: AutocompleteOption | null): string => {
-        if (!item) {
-            return "";
-        }
+        const itemToString = (item: AutocompleteOption | null): string => {
+            if (!item) {
+                return "";
+            }
 
-        return item.label;
-    };
+            return item.label;
+        };
 
-    const {
-        isOpen,
-        highlightedIndex,
-        getMenuProps,
-        getInputProps,
-        getItemProps,
-        getComboboxProps,
-        inputValue,
-        openMenu,
-    } = useCombobox({
-        items: options,
-        itemToString,
-        onSelectedItemChange: (changes: Partial<UseComboboxState<AutocompleteOption>>) => {
-            onChange && changes.selectedItem && onChange(changes.selectedItem);
-            onSelectedItemChange && onSelectedItemChange(changes);
-        },
-    });
+        const {
+            isOpen,
+            highlightedIndex,
+            getMenuProps,
+            getInputProps,
+            getItemProps,
+            getComboboxProps,
+            inputValue,
+            openMenu,
+        } = useCombobox({
+            items: options,
+            itemToString,
+            onSelectedItemChange: (changes: Partial<UseComboboxState<AutocompleteOption>>) => {
+                onChange && changes.selectedItem && onChange(changes.selectedItem);
+                onSelectedItemChange && onSelectedItemChange(changes);
+            },
+        });
 
-    const onInputFocus = (event: React.FocusEvent<HTMLInputElement>) => {
-        openMenu();
-        onFocus && onFocus(event);
-    };
+        const onInputFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+            openMenu();
+            onFocus && onFocus(event);
+        };
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value = event.target.value;
-        onChange && onChange({ value, label: value });
-    };
+        const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+            const value = event.target.value;
+            onChange && onChange({ value, label: value });
+        };
 
-    return (
-        <div {...getComboboxProps()}>
-            <Input
-                {...getInputProps({
-                    onFocus: onInputFocus,
-                    value: value ? value.label : undefined,
-                    onChange: handleInputChange,
-                    ...otherInputProps,
-                })}
-            />
-            <DropDown {...getMenuProps()}>
-                {isOpen &&
-                    props.options.reduce((render: JSX.Element[], option: AutocompleteOption, index: number) => {
-                        if (option.label.toLowerCase().includes(inputValue.toLowerCase())) {
-                            render.push(
-                                <DropDownOption
-                                    key={option.value + option.label + index}
-                                    highlight={highlightedIndex === index}
-                                    {...getItemProps({ item: option, index: index })}
-                                >
-                                    {option.label}
-                                </DropDownOption>,
-                            );
-                        }
-                        return render;
-                    }, [])}
-            </DropDown>
-        </div>
-    );
-};
+        return (
+            <div {...getComboboxProps()}>
+                <Input
+                    {...getInputProps({
+                        onFocus: onInputFocus,
+                        value: value ? value.label : undefined,
+                        onChange: handleInputChange,
+                        refKey: "ref",
+                        ...otherInputProps,
+                        ref,
+                    })}
+                />
+                <DropDown {...getMenuProps()}>
+                    {isOpen &&
+                        props.options.reduce((render: JSX.Element[], option: AutocompleteOption, index: number) => {
+                            if (option.label.toLowerCase().includes(inputValue.toLowerCase())) {
+                                render.push(
+                                    <DropDownOption
+                                        key={option.value + option.label + index}
+                                        highlight={highlightedIndex === index}
+                                        {...getItemProps({ item: option, index: index })}
+                                    >
+                                        {option.label}
+                                    </DropDownOption>,
+                                );
+                            }
+                            return render;
+                        }, [])}
+                </DropDown>
+            </div>
+        );
+    },
+);
 
 export default Autocomplete;
