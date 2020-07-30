234827784028d453d8ce5cdf69c9fa99cb8d270c
Charlie <itscharlieliu@gmail.com>
2020-07-30 16:41:09 -0700

    Add ability to delete transaction

--- src/components/screens/TransactionsScreen.tsx
+++ src/components/screens/TransactionsScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from "react-redux";
+import { connect, ResolveThunks } from "react-redux";
 import styled from "styled-components";
 
 import { theme } from "../../defs/theme";
@@ -8,9 +8,22 @@ import ApplicationState from "../../store";
 import { Transaction } from "../../store/transactions/transactionInterfaces";
 import GridHeaderContainer from "../common/containers/GridHeaderContainer";
 import ScreenContainer from "../common/containers/ScreenContainer";
+import { deleteTransaction } from "../../store/transactions/transactionActions";
+import { ReactComponent as Trash } from "../../resources/images/trash.svg";
+import Button from "../common/Button";
 
 interface StateProps {
     transactions: Transaction[];
+}
+
+interface DispatchProps {
+    deleteTransaction: typeof deleteTransaction;
+}
+
+type AllProps = StateProps & ResolveThunks<DispatchProps>;
+
+interface TransactionsRowProps extends Transaction {
+    onDelete: () => void;
 }
 
 const TransactionsContainer = styled.div`
@@ -19,10 +32,17 @@ const TransactionsContainer = styled.div`
     grid-template-columns: auto 10% 10% 10% 10% 10% 15%;
 `;
 
-const TransactionsRowText = styled.span`
+const TransactionContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
     font-size: ${theme.font.size.small};
     padding: 16px;
     border-top: 1px solid ${theme.palette.divider.main};
+`;
+
+const TransactionRowButton = styled(Button)`
+    margin: -16px 0 -16px 16px;
 `;
 
 const TransactionsHeader = (): JSX.Element => {
@@ -39,30 +59,37 @@ const TransactionsHeader = (): JSX.Element => {
     );
 };
 
-const TransactionsRow = (props: Transaction): JSX.Element => {
+const TransactionsRow = (props: TransactionsRowProps): JSX.Element => {
     const inFlow = props.activity > 0 ? props.activity : 0;
     const outFlow = props.activity < 0 ? -props.activity : 0;
 
     return (
         <>
-            <TransactionsRowText>{props.payee}</TransactionsRowText>
-            <TransactionsRowText>{props.account}</TransactionsRowText>
-            <TransactionsRowText>{props.category}</TransactionsRowText>
-            <TransactionsRowText>{t("fullDate", { date: props.date })}</TransactionsRowText>
-            <TransactionsRowText>{inFlow}</TransactionsRowText>
-            <TransactionsRowText>{outFlow}</TransactionsRowText>
-            <TransactionsRowText>{props.note}</TransactionsRowText>
+            <TransactionContainer>
+                {props.payee}
+                <TransactionRowButton icon={<Trash />} onClick={() => props.onDelete()} flat />
+            </TransactionContainer>
+            <TransactionContainer>{props.account}</TransactionContainer>
+            <TransactionContainer>{props.category}</TransactionContainer>
+            <TransactionContainer>{t("fullDate", { date: props.date })}</TransactionContainer>
+            <TransactionContainer>{inFlow}</TransactionContainer>
+            <TransactionContainer>{outFlow}</TransactionContainer>
+            <TransactionContainer>{props.note}</TransactionContainer>
         </>
     );
 };
 
-const TransactionsScreen = (props: StateProps): JSX.Element => {
+const TransactionsScreen = (props: AllProps): JSX.Element => {
     return (
         <ScreenContainer>
             <TransactionsContainer>
                 <TransactionsHeader />
                 {props.transactions.map((transaction: Transaction, index: number) => (
-                    <TransactionsRow key={"transaction" + index} {...transaction} />
+                    <TransactionsRow
+                        key={"transaction" + index}
+                        onDelete={() => props.deleteTransaction(index)}
+                        {...transaction}
+                    />
                 ))}
             </TransactionsContainer>
         </ScreenContainer>
@@ -73,4 +100,8 @@ const mapStateToProps = (state: ApplicationState): StateProps => ({
     transactions: state.transaction.transactions,
 });
 
-export default connect(mapStateToProps, {})(TransactionsScreen);
+const mapDispatchToProps: DispatchProps = {
+    deleteTransaction,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TransactionsScreen);
