f0133db0be3c6b28a1e6c699871439b5b463f434
Charlie <itscharlieliu@gmail.com>
2020-09-25 15:49:17 -0700

    Add test for if adding budget fails

--- src/store/__tests__/transactions.test.ts
+++ src/store/__tests__/transactions.test.ts
@@ -3,11 +3,20 @@ import configureMockStore from "redux-mock-store";
 import thunk, { ThunkDispatch } from "redux-thunk";
 
 import getMonthCode from "../../utils/getMonthCode";
-import { AccountType, UPDATE_ACCOUNT_SUCCESS, UPDATING_ACCOUNT } from "../accounts/accountsInterfaces";
-import { SET_TOTAL_BUDGET_SUCCESS, SETTING_TOTAL_BUDGET } from "../budget/budgetInterfaces";
+import {
+    AccountType,
+    UPDATE_ACCOUNT_SUCCESS,
+    UPDATING_ACCOUNT,
+    UPDATE_ACCOUNT_FAILURE,
+} from "../accounts/accountsInterfaces";
+import { SET_TOTAL_BUDGET_SUCCESS, SETTING_TOTAL_BUDGET, SET_TOTAL_BUDGET_FAILURE } from "../budget/budgetInterfaces";
 import ApplicationState from "../index";
 import { addTransaction } from "../transactions/transactionActions";
-import { UPDATE_TRANSACTIONS_SUCCESS, UPDATING_TRANSACTIONS } from "../transactions/transactionInterfaces";
+import {
+    UPDATE_TRANSACTIONS_FAILURE,
+    UPDATE_TRANSACTIONS_SUCCESS,
+    UPDATING_TRANSACTIONS,
+} from "../transactions/transactionInterfaces";
 
 type Dispatch = ThunkDispatch<ApplicationState, null, AnyAction>;
 
@@ -62,6 +71,50 @@ describe("transactions actions", () => {
         expect(actions[4].type).toBe(UPDATE_ACCOUNT_SUCCESS);
 
         expect(actions[5].type).toBe(UPDATE_TRANSACTIONS_SUCCESS);
+    });
+
+    it("does not try to update accounts unless budget actions succeed", async () => {
+        const store = mockStore({
+            transaction: {
+                transactions: [],
+                isUpdatingTransactions: false,
+                error: null,
+            },
+            accounts: {
+                allAccounts: [
+                    {
+                        name: "test account",
+                        type: AccountType.budgeted,
+                        balance: 100,
+                    },
+                ],
+            },
+            budget: {
+                totalBudget: {
+                    [monthCode]: {
+                        "test group": {
+                            "test category": {
+                                budgeted: 70,
+                                activity: -15,
+                            },
+                        },
+                    },
+                },
+            },
+        });
+
+        await store.dispatch(
+            addTransaction("invalid payee payee", "invalid account", "invalid category", testDate, -70),
+        );
+        const actions = store.getActions();
+
+        expect(actions[0].type).toBe(UPDATING_TRANSACTIONS);
+
+        // First set budget accurately
+        expect(actions[1].type).toBe(SETTING_TOTAL_BUDGET);
+        expect(actions[2].type).toBe(SET_TOTAL_BUDGET_FAILURE);
+
+        expect(actions[3].type).toBe(UPDATE_TRANSACTIONS_FAILURE);
     });
 });
 
