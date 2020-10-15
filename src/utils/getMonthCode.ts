c09d51b06828822f2dea5d2cbf4aa240b7a70753
Charlie <itscharlieliu@gmail.com>
2020-10-14 20:01:43 -0700

    WIP Refactor month code to not use classes

--- src/utils/getMonthCode.ts
+++ src/utils/getMonthCode.ts
@@ -1,22 +1,38 @@
-export class MonthCode {
-    public readonly month: number = 0;
-    public readonly year: number = 0;
+// export class MonthCode {
+//     public readonly month: number = 0;
+//     public readonly year: number = 0;
+//
+//     constructor(date: Date = new Date()) {
+//         this.month = date.getMonth();
+//         this.year = date.getFullYear();
+//     }
+//
+//     public toString(): string {
+//         return `${this.year}${this.month.toString().padStart(2, "0")}`;
+//     }
+//
+//     public getNext(): MonthCode {
+//         return new MonthCode(new Date(this.year, this.month + 1));
+//     }
+//
+//     public getPrev(): MonthCode {
+//         return new MonthCode(new Date(this.year, this.month - 1));
+//     }
+// }
 
-    constructor(date: Date = new Date()) {
-        this.month = date.getMonth();
-        this.year = date.getFullYear();
-    }
+const MIN_MONTH = 0;
+const MAX_MONTH = 11;
 
-    public toString(): string {
-        return `${this.year}${this.month.toString().padStart(2, "0")}`;
-    }
+interface MonthCode {
+    month: number;
+    year: number;
+}
 
-    public getNext(): MonthCode {
-        return new MonthCode(new Date(this.year, this.month + 1));
-    }
+// TODO Refactor this to not use class
 
-    public getPrev(): MonthCode {
-        return new MonthCode(new Date(this.year, this.month - 1));
+const isValidMonthCode = (monthCode: MonthCode) {
+    if (monthCode.month > MAX_MONTH || monthCode.month < MIN_MONTH) {
+        return false;
     }
 }
 
