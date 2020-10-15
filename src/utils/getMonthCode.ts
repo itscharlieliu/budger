2ad39345cbe641818b91746b3ae0ca65a7820ea9
Charlie <itscharlieliu@gmail.com>
2020-10-15 14:31:01 -0700

    Refactor Month code again to be more functional

--- src/utils/getMonthCode.ts
+++ src/utils/getMonthCode.ts
@@ -1,39 +1,43 @@
-// export class MonthCode {
-//     public readonly month: number = 0;
-//     public readonly year: number = 0;
-//
-//     constructor(date: Date = new Date()) {
-//         this.month = date.getMonth();
-//         this.year = date.getFullYear();
-//     }
-//
-//     public toString(): string {
-//         return `${this.year}${this.month.toString().padStart(2, "0")}`;
-//     }
-//
-//     public getNext(): MonthCode {
-//         return new MonthCode(new Date(this.year, this.month + 1));
-//     }
-//
-//     public getPrev(): MonthCode {
-//         return new MonthCode(new Date(this.year, this.month - 1));
-//     }
-// }
-
 const MIN_MONTH = 0;
 const MAX_MONTH = 11;
 
-interface MonthCode {
+// I would be surprised if this app is still being used in the year 2999
+const MIN_YEAR = 2000;
+const MAX_YEAR = 2999;
+
+export interface MonthCode {
     month: number;
     year: number;
 }
 
-// TODO Refactor this to not use class
+export const getNextMonthCode = (monthCode: MonthCode): MonthCode => {
+    const nextMonthCodeDate = new Date(monthCode.year, monthCode.month + 1);
 
-const isValidMonthCode = (monthCode: MonthCode) {
+    return { month: nextMonthCodeDate.getMonth(), year: nextMonthCodeDate.getFullYear() };
+};
+
+export const getPrevMonthCode = (monthCode: MonthCode): MonthCode => {
+    const nextMonthCodeDate = new Date(monthCode.year, monthCode.month - 1);
+
+    return { month: nextMonthCodeDate.getMonth(), year: nextMonthCodeDate.getFullYear() };
+};
+
+export const getMonthCodeString = (monthCode: MonthCode): string => {
+    return `${monthCode.year}${monthCode.month.toString().padStart(2, "0")}`;
+};
+
+export const validateMonthCode = (monthCode: MonthCode): boolean => {
+    if (isNaN(monthCode.month)) {
+        return false;
+    }
+
     if (monthCode.month > MAX_MONTH || monthCode.month < MIN_MONTH) {
         return false;
     }
-}
 
-export default MonthCode;
+    if (isNaN(monthCode.year)) {
+        return false;
+    }
+
+    return !(monthCode.year > MAX_YEAR || monthCode.year < MIN_YEAR);
+};
