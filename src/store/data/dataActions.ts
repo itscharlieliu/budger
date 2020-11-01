187e4d3470744b33ac0d43db9c04b032304d1ae0
Charlie <itscharlieliu@gmail.com>
2020-11-01 12:03:26 -0800

    Parse transaction rows

--- src/store/data/dataActions.ts
+++ src/store/data/dataActions.ts
@@ -1,18 +1,22 @@
 import { ThunkAction, ThunkDispatch } from "redux-thunk";
 import { GenericDataAction, IMPORT_DATA_SUCCESS, ImportDataSuccessAction, IMPORTING_DATA } from "./dataInterfaces";
 import ApplicationState from "../index";
-import Papa, { ParseConfig } from "papaparse";
+import Papa, { ParseConfig, ParseResult } from "papaparse";
 
 export type GenericDataThunkAction = ThunkAction<Promise<GenericDataAction>, ApplicationState, null, GenericDataAction>;
 
-export const importData = (blob: File): GenericDataThunkAction => {
+export const importTransactionData = (blob: File): GenericDataThunkAction => {
     return async (dispatch: ThunkDispatch<ApplicationState, null, GenericDataAction>): Promise<GenericDataAction> => {
         dispatch({ type: IMPORTING_DATA });
 
-        return new Promise<GenericDataAction>((resolve) => {
+        return new Promise<GenericDataAction>((resolve: (value?: GenericDataAction) => void) => {
             const config: ParseConfig = {
-                complete: (results: unknown) => {
+                complete: (results: ParseResult<string[]>) => {
                     console.log(results);
+
+                    for (const row of results.data) {
+                        console.log(row);
+                    }
 
                     resolve(dispatch({ type: IMPORT_DATA_SUCCESS }));
                 },
