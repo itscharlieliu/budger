bd428f9c5084ceeb6c351e33a14d8dae5e34a03e
Charlie <itscharlieliu@gmail.com>
2025-11-02 12:02:36 -0500

    clean up transaction interfaces

--- client/app/store/transactions/transactionInterfaces.ts
+++ client/app/store/transactions/transactionInterfaces.ts
@@ -1,9 +1,3 @@
-import { Action } from "redux";
-
-export const UPDATING_TRANSACTIONS = "UPDATING_TRANSACTIONS";
-export const UPDATE_TRANSACTIONS_SUCCESS = "UPDATE_TRANSACTIONS_SUCCESS";
-export const UPDATE_TRANSACTIONS_FAILURE = "UPDATE_TRANSACTIONS_FAILURE";
-
 export interface Transaction {
     id?: string;
     account: string; // Account name for display
@@ -20,20 +14,3 @@ export interface TransactionState {
     isUpdatingTransactions: boolean;
     error: Error | null;
 }
-
-export interface UpdatingTransactionAction extends Action<typeof UPDATING_TRANSACTIONS> {}
-
-export interface UpdateTransactionSuccessAction extends Action<typeof UPDATE_TRANSACTIONS_SUCCESS> {
-    transactions: Transaction[];
-}
-
-export interface UpdateTransactionFailureAction extends Action<typeof UPDATE_TRANSACTIONS_FAILURE> {
-    error: Error;
-}
-
-export type GenericUpdateTransactionAction =
-    | UpdatingTransactionAction
-    | UpdateTransactionSuccessAction
-    | UpdateTransactionFailureAction;
-
-export type GenericTransactionAction = GenericUpdateTransactionAction;
